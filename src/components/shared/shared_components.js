c9c6d94c38a41a3d3258d2ae9fd582e40656a370
Alex Caulfield <alex.caulfield28@gmail.com>
2021-04-26 21:14:01 -0400

    formatting

--- src/components/shared/shared_components.js
+++ src/components/shared/shared_components.js
@@ -47,4 +47,11 @@ const DarkModeIcon = styled(Icon)`
   color: var(--textNormal) !important;
 `;
 
-export { DarkModeHeader, DarkModeCard, DarkModeButton, DarkModeLabel, DarkModeItem, DarkModeIcon };
+export {
+  DarkModeHeader,
+  DarkModeCard,
+  DarkModeButton,
+  DarkModeLabel,
+  DarkModeItem,
+  DarkModeIcon,
+};
